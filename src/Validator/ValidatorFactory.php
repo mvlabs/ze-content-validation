44d086e4af2cd175082a809e71e57f177ddceef8
Diego Drigani <d.drigani@mvassociati.it>
2016-12-28 11:17:31 +0100

    removed debug info

--- src/Validator/ValidatorFactory.php
+++ src/Validator/ValidatorFactory.php
@@ -15,8 +15,7 @@ class ValidatorFactory
 {
     public function __invoke(ContainerInterface $container)
     {
-        echo 'popio';
-        var_dump($container->get('InputFilterManager'));
+        //$container->get('InputFilterManager');
 
         return new Validator(
             $container->get(OptionsExtractor::class),
