99d02ae17f05340b268778939a4821de72ce904c
Diego Drigani <d.drigani@mvassociati.it>
2017-02-15 12:34:04 +0100

    added ConfigProvider + dependencies configured

---
+++ src/ConfigProvider.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * ze-content-validation (https://github.com/mvlabs/ze-content-validation)
+ *
+ * @copyright Copyright (c) 2017 MVLabs(http://mvlabs.it)
+ * @license MIT
+ */
+namespace ZE\ContentValidation;
+
+use Zend\Config\Factory;
+use Zend\Stdlib\Glob;
+
+/**
+ * Class ConfigProvider
+ * @package ZE\ContentValidation
+ */
+class ConfigProvider
+{
+    /**
+     * @return mixed
+     */
+    public function __invoke()
+    {   return [];
+        return Factory::fromFiles(Glob::glob(__DIR__ . '/../config/{,*.}config.php', Glob::GLOB_BRACE));
+    }
+}
